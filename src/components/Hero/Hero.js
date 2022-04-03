88bf6618e74c84001cd90fac2a3b1017f6a665fc
SamarpitSantoki <samarpit.santoki@gmail.com>
2022-04-03 15:30:29 +0530

    Added My CV

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
+import Link from "next/link";
 
 const Hero = (props) => (
   <Section row nopadding nomargin>
@@ -12,10 +17,18 @@ const Hero = (props) => (
         My Portfolio
       </SectionTitle>
       <SectionText>
-        Hi There✌️ ,I'm Samarpit Santoki, a student at Charusat University. My current interests are Web Development, Game Development and learning Linux(I'm obsessed with it at the moment).Also I love to know more about latest technology and to watch anime in my free time.
-        I am also developing interest in open-source projects.
+        Hi There✌️ ,I'm Samarpit Santoki, a student at Charusat University. My
+        current interests are Web Development, Game Development and learning
+        Linux(I'm obsessed with it at the moment).Also I love to know more about
+        latest technology and to watch anime in my free time. I am also
+        developing interest in open-source projects.
       </SectionText>
-      <Button onClick={() => window.location = '#'}>My CV</Button>
+      <a
+        href="https://drive.google.com/viewerng/viewer?url=https://samarpitsantoki.me/SamarpitSantoki.pdf"
+        target="__blank"
+      >
+        <Button>My CV</Button>
+      </a>
     </LeftSection>
   </Section>
 );
--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,49 +1,60 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact, DiZend } from "react-icons/di";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
+const Technologies = () => (
   <Section id="technology">
-    <SectionDivider/>
-    <br/>
+    <SectionDivider />
+    <br />
     <SectionTitle>Technology</SectionTitle>
-     <SectionText>
-       I have worked with a bunch of technology in Web development from back-end to design.
-     </SectionText>
-     <List>
-       <ListItem>
-         <DiReact size="3rem"/>
-         <ListContainer>
-           <ListTitle>Front-End</ListTitle>
-           <ListParagraph>
-             Experince with <br/>
-             React and Hbs
-           </ListParagraph>
-         </ListContainer>
-       </ListItem>
-       <ListItem>
-         <DiFirebase size="3rem"/>
-         <ListContainer>
-           <ListTitle>Back-End</ListTitle>
-           <ListParagraph>
-             Experince with <br/>
-             Express and MongoDB
-           </ListParagraph>
-         </ListContainer>
-       </ListItem>
-       <ListItem>
-         <DiZend size="3rem"/>
-         <ListContainer>
-           <ListTitle>UI/UX</ListTitle>
-           <ListParagraph>
-             Experince with <br/>
-             Figma
-           </ListParagraph>
-         </ListContainer>
-       </ListItem>
-     </List>
-
+    <SectionText>
+      I have worked with a bunch of technology in Web development from back-end
+      to design.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experince with <br />
+            Next and ReactJs
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Experince with <br />
+            Express and MongoDB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experince with <br />
+            Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
   </Section>
 );
 
